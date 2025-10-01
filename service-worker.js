c5780657d92dc0b564563389a6c4d075e269a912
romahastr-cell <romaha.str@gmail.com>
2025-10-01 14:49:57 +0800

    service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE = 'sk-v1';
+const ASSETS = [
+  '/', '/index.html', '/logo.jpg', '/og-image.jpg', '/favicon.ico', '/manifest.json'
+];
+
+self.addEventListener('install', e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate', e=>{
+  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))); 
+  self.clients.claim();
+});
+self.addEventListener('fetch', e=>{
+  const url = new URL(e.request.url);
+  if(url.origin === location.origin){
+    e.respondWith(caches.match(e.request).then(res=> res || fetch(e.request).then(r=>{
+      const copy = r.clone(); caches.open(CACHE).then(c=>c.put(e.request, copy)); return r;
+    })));
+  } else {
+    e.respondWith(fetch(e.request).catch(()=>caches.match('/index.html')));
+  }
+});
